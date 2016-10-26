86de5325c00c594da3c8c9c89e572ddae5c3723c
James Dumay <jameswdumay@gmail.com>
2016-10-27 10:10:33 +1100

    ClassicDisplayURLProvider is now its own extension

--- src/main/java/org/jenkinsci/plugins/displayurlapi/DisplayURLProvider.java
+++ src/main/java/org/jenkinsci/plugins/displayurlapi/DisplayURLProvider.java
@@ -1,15 +1,13 @@
 package org.jenkinsci.plugins.displayurlapi;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.google.common.collect.Iterables;
+import hudson.ExtensionList;
 import hudson.ExtensionPoint;
 import hudson.Util;
-import hudson.model.Item;
 import hudson.model.Job;
-import hudson.model.Project;
 import hudson.model.Run;
-import hudson.tasks.junit.TestResult;
-import hudson.tasks.test.AbstractTestResultAction;
-import hudson.tasks.test.TestObject;
 import jenkins.model.Jenkins;
 
 /**
@@ -17,8 +15,6 @@ import jenkins.model.Jenkins;
  * Extensible to allow plugins to override common URLs (e.g. Blue Ocean or another future secondary UI)
  */
 public abstract class DisplayURLProvider implements ExtensionPoint {
-
-    private static final ClassicDisplayURLProvider CLASSIC_DISPLAY_URL_PROVIDER = new ClassicDisplayURLProvider();
 
     /**
      * Returns the first {@link DisplayURLProvider} found
@@ -29,7 +25,18 @@ public abstract class DisplayURLProvider implements ExtensionPoint {
         if (jenkins == null) {
             throw new IllegalStateException("Jenkins has not started");
         }
-        return Iterables.getFirst(jenkins.getExtensionList(DisplayURLProvider.class), CLASSIC_DISPLAY_URL_PROVIDER);
+        ExtensionList<DisplayURLProvider> allProviders = jenkins.getExtensionList(DisplayURLProvider.class);
+        DisplayURLProvider defaultDisplayURLProvider = Iterables.find(allProviders, new Predicate<DisplayURLProvider>() {
+            @Override
+            public boolean apply(DisplayURLProvider input) {
+                return input.getClass().equals(ClassicDisplayURLProvider.class);
+            }
+        });
+        Iterable<DisplayURLProvider> availableDisplayURLProviders = Iterables.filter(
+                allProviders,
+                Predicates.not(Predicates.equalTo(defaultDisplayURLProvider))
+        );
+        return Iterables.getFirst(availableDisplayURLProviders, defaultDisplayURLProvider);
     }
 
     /** Fully qualified URL for the Root display URL */
@@ -45,6 +52,9 @@ public abstract class DisplayURLProvider implements ExtensionPoint {
         return Util.encode(root);
     }
 
+    /** Name of this display URL provider */
+    public abstract String getName();
+
     /** Fully qualified URL for a Run */
     public abstract String getRunURL(Run<?, ?> run);
 
@@ -56,52 +66,4 @@ public abstract class DisplayURLProvider implements ExtensionPoint {
 
     /** Fully qualified URL to the test details page for a given test result */
     public abstract String getTestUrl(hudson.tasks.test.TestResult result);
-
-    /** URL Factory for the Classical Jenkins UI */
-    static class ClassicDisplayURLProvider extends DisplayURLProvider {
-        @Override
-        public String getRunURL(Run<?, ?> run) {
-            return getRoot() + Util.encode(run.getUrl());
-        }
-
-        @Override
-        public String getChangesURL(Run<?, ?> run) {
-            return getJobURL(run.getParent()) + "changes";
-        }
-
-        @Override
-        public String getJobURL(Job<?, ?> project) {
-            return getRoot() + Util.encode(project.getUrl());
-        }
-
-        @Override
-        public String getTestUrl(hudson.tasks.test.TestResult result) {
-            String buildUrl = getRunURL(result.getRun());
-            AbstractTestResultAction action = result.getTestResultAction();
-
-            TestObject parent = result.getParent();
-            TestResult testResultRoot = null;
-            while(parent != null) {
-                if (parent instanceof TestResult) {
-                    testResultRoot = (TestResult) parent;
-                    break;
-                }
-                parent = parent.getParent();
-            }
-
-            String testUrl = action.getUrlName()
-                    + (testResultRoot != null ? testResultRoot.getUrl() : "")
-                    + result.getUrl();
-
-            String[] pathComponents = testUrl.split("/");
-            StringBuilder buf = new StringBuilder();
-            for (String c : pathComponents) {
-                buf.append(Util.rawEncode(c)).append('/');
-            }
-            // remove last /
-            buf.deleteCharAt(buf.length() - 1);
-
-            return buildUrl + buf.toString();
-        }
-    }
 }
